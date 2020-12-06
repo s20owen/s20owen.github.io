66d1c3e4e9d42f7c0ef0d1161ccfeee17b999acd
s20owen <71678206+s20owen@users.noreply.github.com>
2020-12-06 18:00:37 -0500

    Add files via upload

---
+++ pages/password/js/main.js
@@ -0,0 +1,8 @@
+window.onload = () => {  
+'use strict';     
+if ('serviceWorker' in navigator) {     
+navigator.serviceWorker  
+.register('/sw.js'); 
+console.log("service worker ready/Installed")
+} 
+}
