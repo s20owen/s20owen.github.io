0c581fc24686796795159e47f03ecb1f00b8d190
s20owen <71678206+s20owen@users.noreply.github.com>
2025-05-12 20:18:16 -0400

    Add files via upload

--- pages/Circle Chain/levelData.js
+++ pages/Circle Chain/levelData.js
@@ -6,7 +6,7 @@ const LEVELS = [
   { redCount: 12, greenCount: 6, yellowCount: 1, purpleCount: 0, blueCount: 1, targetHits: 15 },
   { redCount: 12, greenCount: 4, yellowCount: 4, purpleCount: 0, blueCount: 2, targetHits: 17 },
   { redCount: 2, greenCount: 2, yellowCount: 12, purpleCount: 0, blueCount: 1, targetHits: 12 },
-  { redCount: 0, greenCount: 25, yellowCount: 0, purpleCount: 0, blueCount: 0, targetHits: 27 },
+  { redCount: 0, greenCount: 30, yellowCount: 0, purpleCount: 0, blueCount: 0, targetHits: 30 },
   { redCount: 1, greenCount: 0, yellowCount: 0, purpleCount: 0, blueCount: 20, targetHits: 1 },
   { redCount: 8, greenCount: 4, yellowCount: 4, purpleCount: 1, blueCount: 1, targetHits: 15 },
 ];
